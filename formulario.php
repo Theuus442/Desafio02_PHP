a7826dcb409050d9c726c8c610e7800c716ae6e4
Matheus Alves <matheusalvesantana@hotmail.com>
2025-06-26 09:40:00 -0300

    Formulario das informações do livro criado

---
+++ formulario.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Informações Livro</title>
+</head>
+<body>
+    <h2>Informe os dados do livro</h2>
+    <form method="post" action="processa.php">
+        Título do livro: <input type="text" name="tituloLivro">
+        Autor <input type="text" name="autorLivro">
+        Ano do livro <input type="text" name="anoLivro">
+        <input type="submit" value="enviar Informação">
+    </form>
+</body>
+</html>
