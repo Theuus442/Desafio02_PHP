c34cd04dae17f0aa6cc42999e0e8ad2d3d84d2c9
Matheus Alves <matheusalvesantana@hotmail.com>
2025-06-26 12:42:38 -0300

    A função validar campo dos textos foi criada

--- funcoes.php
+++ funcoes.php
@@ -3,4 +3,13 @@
 function verificaAntigoOuAtual($ano){
     return ($ano < 2000) ? "antigo" : "atual";
 }
+
+
+function validarCampoTexto($inforPreenchida, $nomeCampo, &$erros){
+    if(empty(trim($inforPreenchida))){
+        $erros[] = "O campo $nomeCampo é obrigatório.";
+    }
+}
+
+
 ?>
