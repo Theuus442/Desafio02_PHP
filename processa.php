4a57125a620e2590db052900236283a3366c7357
Matheus Alves <matheusalvesantana@hotmail.com>
2025-06-26 10:34:08 -0300

    Corrigir erro no processamento para exibir dados corretamente na listagem

--- processa.php
+++ processa.php
@@ -1,11 +1,13 @@
 <?php
 
+session_start();
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $tituloLivro = filter_input(INPUT_POST, "nomeLivro");
+    $tituloLivro = filter_input(INPUT_POST, "tituloLivro", FILTER_SANITIZE_SPECIAL_CHARS);
     $autorLivro = filter_input(INPUT_POST, "autorLivro", FILTER_SANITIZE_SPECIAL_CHARS);
     $anoLivro = filter_input(INPUT_POST, "anoLivro", FILTER_VALIDATE_INT);
 
-    if ($tituloLivro || $autorLivro || $anoLivro){
+    if ($tituloLivro && $autorLivro && $anoLivro){
         if(!isset($_SESSION['livros'])){
             $_SESSION['livros'] = [];
         }
@@ -16,15 +18,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
             'anoLivro' => $anoLivro
         ];
 
-        echo "Livro adidicionado com suceso!<br><br>";
-        echo "<a href='formulario.php'>Adicionar outras pessoa</a><br>";
-        echo "<a href='lista.php'>Ver lista de livros";
+        echo "Livro adicionado com sucesso!<br><br>";
+        echo "<a href='formulario.php'>Adicionar outros livros</a><br>";
+        echo "<a href='lista.php'>Ver lista de livros</a>";
     } else{
-        echo "Por favor, preencha todos os campos corretamente";
+        echo "Por favor, preencha todos os campos corretamente<br>";
         echo "<a href='formulario.php'>Voltar</a>";
     }
 } else{
-    echo "Acesso inválido, pois só está permitido o formulario e a lista!";
+    echo "Acesso inválido, pois só está permitido o formulário e a lista!";
 }
 
 ?>
