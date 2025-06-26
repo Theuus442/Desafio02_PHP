b70ade55aa5d189cd7f96cd3b27b5cf69f8e5522
Matheus Alves <matheusalvesantana@hotmail.com>
2025-06-26 10:54:37 -0300

    Funcionalidade adicionada de listar antigo ou atual

--- lista.php
+++ lista.php
@@ -1,15 +1,16 @@
 <?php
 session_start();
-
+require_once("funcoes.php");
 
 if (isset($_SESSION['livros']) && count($_SESSION['livros']) > 0){
     echo "<h2>Lista de Livros</h2>";
     echo"<ul>";
     foreach($_SESSION['livros'] as $livro){
-        echo "<li>{$livro['tituloLivro']} - {$livro['autorLivro']} - {$livro['anoLivro']}</li>";
+        $status = verificaAntigoOuAtual($livro['anoLivro']);
+        echo "<li>{$livro['tituloLivro']} - {$livro['autorLivro']} - {$livro['anoLivro']} - O livro '{$livro['tituloLivro']}' é $status.</li>";
     } echo "</ul>";
 } else{
     echo "Nenhum livro cadastrado!";
-} echo "<br><a href='formulario.php'>voltar ao formulário</a>";
+} echo "<br><a href='formulario.php'>Voltar ao formulário</a>";
 
 ?>
